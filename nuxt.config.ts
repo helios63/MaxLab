03928ffdb37cebd6ea892359a9e6370b321325a0
Maxime Mondet <maxime@llos.co>
2026-06-26 18:25:38 +0200

    G tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-1Q0VHVVQLH',
+          async: true,
+        },
+        {
+          innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-1Q0VHVVQLH');`,
+        },
+      ],
+    },
+  },
 })
